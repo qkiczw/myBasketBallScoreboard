166c3df330bc8e0960b2f302b7929ce199bd7e0d
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2020-11-21 18:21:59 +0100

    I added code to increase and decrase minutes

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const BTN_START = document.querySelector('.btn__start');
 const BTN_PAUSE= document.querySelector('.btn__pause');
+const BTN_INCREASE_MINUTES = document.querySelector('.btn__increase__minutes')
+const BTN_DECREASE_MINUTES = document.querySelector('.btn__decrease__minutes')
+const BTN_INCREASE_SECONDS = document.querySelector('.btn__increase__seconds')
+const BTN_DECREASE_SECONDS = document.querySelector('.btn__decrease__seconds')
+
 let countDownElMinutes = document.querySelector('.countdown__timer__current--minutes');
 let countDownElSeconds = document.querySelector('.countdown__timer__current--seconds');
 
@@ -9,9 +14,7 @@ let defaultSartSeconds = 0;
 countDownElMinutes.innerHTML = defaultSartMinutes < 10 ? `0${defaultSartMinutes}` : defaultSartMinutes;
 countDownElSeconds.innerHTML = defaultSartSeconds < 10 ? `0${defaultSartSeconds}` : defaultSartSeconds;
 
-
-const startingMinutes = 0.10;
-let time = startingMinutes * 60;
+let time = defaultSartMinutes * 60;
 
 
 const countDownEl = document.querySelector('.countdown__time'); 
@@ -28,7 +31,6 @@ function updateCountdown() {
     countDownElMinutes.innerHTML = minutes;
     countDownElSeconds.innerHTML = seconds;
 
-    // countDownEl.innerHTML = `${minutes}:${seconds}`;
     time--;
 
     console.log(time)
@@ -46,5 +48,27 @@ BTN_PAUSE.addEventListener('click', function() {
     clearInterval(startCountign);
     BTN_START.disabled = false;
 })
+BTN_INCREASE_MINUTES.addEventListener('click', function() {
+    defaultSartMinutes++
+    countDownElMinutes.innerHTML = defaultSartMinutes < 10 ? `0${defaultSartMinutes}` : defaultSartMinutes;
+    console.log(defaultSartMinutes);
+    time = defaultSartMinutes * 60;
+})
+BTN_DECREASE_MINUTES.addEventListener('click', function() {
+    defaultSartMinutes--
+    defaultSartMinutes < 0 ? defaultSartMinutes = 0 : defaultSartMinutes;
+    countDownElMinutes.innerHTML = defaultSartMinutes < 10 ? `0${defaultSartMinutes}` : defaultSartMinutes;
+    time = defaultSartMinutes * 60;
+})
+BTN_INCREASE_SECONDS.addEventListener('click', function() {
+    defaultSartSeconds++
+    countDownElSeconds.innerHTML = defaultSartSeconds < 10 ? `0${defaultSartSeconds}` : defaultSartSeconds;
+})
+BTN_DECREASE_SECONDS.addEventListener('click', function() {
+    defaultSartSeconds--
+    defaultSartSeconds < 0 ? defaultSartSeconds = 0 : defaultSartSeconds;
+    countDownElSeconds.innerHTML = defaultSartSeconds < 10 ? `0${defaultSartSeconds}` : defaultSartSeconds;
+})
+
 
 
